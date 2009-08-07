9ffff01d286b98071a187fb7d9ab5662d928efac
jkibele <jkibele@localhost>
2009-08-07 20:18:04 +0000

    Using django file fields to manage shapefile uploads and storage now.

--- lingcod/intersection/views.py
+++ lingcod/intersection/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from intersection.models import *
+
+def split_to_single_shapefiles(request, multishape_pk, field_name):
+    if request.user.is_staff():
+        MultiFeatureShapefile.objects.get(pk=multishape_pk).split_to_single_feature_shapefiles(field_name)
