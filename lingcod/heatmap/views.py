b6dc3f667af6b669ffe64945d64c3c5b927839f1
Chad Burt <underbluewaters@gmail.com>
2010-10-07 15:35:07 -0700

    Started new branch for working on spatial content managment features. To be merged into the default branch with version 2.0

---
+++ lingcod/heatmap/views.py
@@ -0,0 +1,99 @@
+# Create your views here.
+from models import create_heatmap
+from lingcod.common.utils import get_array_class
+from lingcod.shapes.views import ShpResponder
+from lingcod.common import default_mimetypes as mimetypes
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest, HttpResponseServerError, HttpResponseForbidden, Http404
+from lingcod.sharing.utils import can_user_view
+import os
+
+def overlap_geotiff(array_id_list_str, user=None):
+    Array = get_array_class()
+    array_set = Array.objects.filter(pk__in=array_id_list_str.split(','))
+    if len(array_set) < 1:
+        raise Http404
+    for array in array_set:
+        viewable, response = can_user_view(Array, array.pk, user)
+        if user and not viewable:
+            return response
+    filenames = []
+    for array in array_set:
+        responder = ShpResponder(array.shapefile_export_query_set)
+        fn = responder('return_file_not_response')
+        filenames.append(fn)
+    temp_geotiff = create_heatmap(filenames)
+    return temp_geotiff
+    
+def overlap_geotiff_response(request, array_id_list_str):
+    import cStringIO
+    buff = cStringIO.StringIO()
+    temp_geotiff = overlap_geotiff(array_id_list_str, request.user)
+    rfile = open(temp_geotiff,'rb')
+    buff.write(rfile.read())
+    buff.flush()
+    stream = buff.getvalue()
+    response = HttpResponse()
+    response['Content-Disposition'] = 'attachment; filename=%s' % os.path.basename(temp_geotiff)
+    response['Content-length'] = str(len(stream))
+    response['Content-Type'] = 'image/geotiffint16'
+    response.write(stream)
+    return response
+
+def overlap_kmz_response(request, array_id_list_str):
+    import cStringIO
+    import shutil
+    import tempfile
+
+    buff = cStringIO.StringIO()
+    temp_geotiff = overlap_geotiff(array_id_list_str, request.user)
+    
+    tempdir = os.path.join(tempfile.gettempdir(),str(array_id_list_str.__hash__()))
+    if os.path.exists(tempdir):
+        shutil.rmtree(tempdir)
+    os.mkdir(tempdir)
+    outrgb = os.path.join(tempdir, "rgb.png")
+    outvrt = os.path.join(tempdir, "rgb.vrt")
+    outkmldir = os.path.join(tempdir, "kmlfiles")
+    if os.path.exists(outkmldir):
+        shutil.rmtree(outkmldir)
+    os.mkdir(outkmldir)
+    outkmz = os.path.join(tempdir, "array_overlap.kmz")
+
+    # Convert to RGB 
+    cmd = "pct2rgb.py -of PNG %s %s" % (temp_geotiff, outrgb)  
+    print cmd
+    print os.popen(cmd).read()
+
+    # Assign a nodata value
+    cmd = "gdal_translate -of vrt -a_nodata 0 %s %s" % (outrgb, outvrt)
+    print cmd
+    print os.popen(cmd).read()
+         
+    # Split into wgs84 tiles
+    cmd = "gdal2tiles.py -k -z 6-10 -p geodetic %s %s" % (outvrt, outkmldir)
+    print cmd
+    print os.popen(cmd).read()
+
+    # Zip up directory into a kmz
+    print "Creating", outkmz
+    from lingcod.common.utils import KMZUtil
+    zu = KMZUtil()
+    filename = outkmz
+    os.chdir(outkmldir)
+    directory = ""
+    zu.toZip(directory, filename)
+
+    if not os.path.exists(outkmz):
+        return HttpResponse("KMZ creation error", status=500)
+    rfile = open(outkmz,'rb')
+    buff.write(rfile.read())
+    buff.flush()
+    stream = buff.getvalue()
+    response = HttpResponse()
+    response['Content-Disposition'] = 'attachment; filename=%s' % os.path.basename(outkmz)
+    response['Content-length'] = str(len(stream))
+    response['Content-Type'] = mimetypes.KMZ
+    response.write(stream)
+    return response
+
+
---
+++ lingcod/layers/models.py
@@ -0,0 +1,149 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User, Group
+from lingcod.sharing.managers import ShareableGeoManager
+
+class PrivateLayerList(models.Model):
+    """Model for storing uploaded restricted-access kml files"""
+    creation_date = models.DateTimeField(auto_now=True) 
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=50,help_text="Layer name as it will appear in the KML tree.",default='')
+    priority = models.FloatField(help_text="Floating point. Higher number = appears higher up on the KML tree.",default=0.0)
+    kml = models.FileField(upload_to='upload/private-kml-layers/', help_text="""
+        KML file (not publically available). This file can use
+        NetworkLinks pointing to remote kml datasets or WMS servers.
+    """, blank=False, max_length=510)
+
+    sharing_groups = models.ManyToManyField(Group,blank=True,null=True,verbose_name="Share layer with the following groups")
+    objects = ShareableGeoManager()
+
+    def __unicode__(self):
+        return "PrivateLayerList %s " % (self.name)
+
+    class Meta:
+        permissions = (
+            ("can_share_privatelayerlist", "Can share private layer list"),
+        )
+    
+class PrivateSuperOverlay(models.Model):
+    """Model for presenting restricted-access multi-file kml trees on disk"""
+    creation_date = models.DateTimeField(auto_now=True) 
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=50,help_text="Layer name as it will appear in the KML tree.",default='')
+    priority = models.FloatField(help_text="Floating point. Higher number = appears higher up on the KML tree.",default=0.0)
+    base_kml = models.FilePathField(path=settings.SUPEROVERLAY_ROOT, match="doc.kml", recursive=True, help_text="""
+        Base KML file of the superoverlay. Must be called 'doc.kml'. This file (and all subsequent files in the tree) must use
+        relative paths.  The user making the request only needs permissions for the base kml. 
+        IMPORTANT: Every file in and below the base kml's directory path is accessible 
+        if the user has proper permissions on the base kml.""")
+
+    sharing_groups = models.ManyToManyField(Group,blank=True,null=True,verbose_name="Share layer with the following groups")
+    objects = ShareableGeoManager()
+
+    def __unicode__(self):
+        return "PrivateSuperOverlay %s " % (self.name)
+
+    class Meta:
+        permissions = (
+            ("can_share_privatesuperoverlay", "Can share private superoverlays"),
+        )
+            
+class UserLayerList(models.Model):
+    """Model used for storing uploaded kml files that list all public layers.
+
+        ======================  ==============================================
+        Attribute               Description
+        ======================  ==============================================
+        ``active``              Whether this kml file represents the currently 
+                                displayed data layers. If set to true and 
+                                another ``UserLayerList`` assigned to the same user
+                                is active, that old list will be deactivated.
+
+        ``user``                Many to many field representing the users assigned to this data layer
+                                
+        ``kml``                 Django `FileField <http://docs.djangoproject.com/en/dev/ref/models/fields/#filefield>`_
+                                that references the actual kml
+                                
+        ``creation_date``       When the layer was created. Is not changed on
+                                updates.
+        ======================  ==============================================
+"""    
+    
+    creation_date = models.DateTimeField(auto_now=True) 
+    
+    user = models.ManyToManyField(User, null=True, blank=True, verbose_name="Data Layer Users")
+    
+    active = models.BooleanField(default=True, help_text="""
+        Checking here indicates that this layer list should be the one used in
+        the application. Copies of other layer lists are retained in the
+        system so you can go back to an old version if necessary.
+    """)
+    
+    kml = models.FileField(upload_to='layers/uploaded-kml/', help_text="""
+        KML file that represents the public layers list. This file can use
+        NetworkLinks pointing to remote kml datasets or WMS servers.
+        For more information on how to create this kml file see the 
+        documentation.
+    """, blank=False, max_length=510)
+    
+    def __unicode__(self):
+        return "UserLayerList, created: %s" % (self.creation_date)
+    
+    #NOTE:  more work needs to be done here so that more than one user can have an active layer
+    def save(self, *args, **kwargs):
+        super(UserLayerList, self).save(*args, **kwargs)
+        #if self.active and UserLayerList.objects.filter(active=True).count() > 1:
+            # Ensure that any previously active layer is deactivated
+            # There can be only one!
+            #UserLayerList.objects.filter(active=True).exclude(pk=self.pk).update(active=False)
+            
+    class Meta:
+        permissions = (
+            ("view_userlayerlist", "Can view user layer list"),
+        )
+
+
+class PublicLayerList(models.Model):
+    """Model used for storing uploaded kml files that list all public layers.
+
+        ======================  ==============================================
+        Attribute               Description
+        ======================  ==============================================
+        ``active``              Whether this kml file represents the currently 
+                                displayed data layers. If set to true and 
+                                another ``PublicLayerList`` is active, that 
+                                old list will be deactivated.
+
+        ``kml``                 Django `FileField <http://docs.djangoproject.com/en/dev/ref/models/fields/#filefield>`_
+                                that references the actual kml
+                                
+        ``creation_date``       When the layer was created. Is not changed on
+                                updates.
+        ======================  ==============================================
+"""    
+    
+    creation_date = models.DateTimeField(auto_now=True) 
+    
+    active = models.BooleanField(default=True, help_text="""
+        Checking here indicates that this layer list should be the one used in
+        the application. Copies of other layer lists are retained in the
+        system so you can go back to an old version if necessary.
+    """)
+    
+    kml = models.FileField(upload_to='layers/uploaded-kml/', help_text="""
+        KML file that represents the public layers list. This file can use
+        NetworkLinks pointing to remote kml datasets or WMS servers.
+        For more information on how to create this kml file see the 
+        documentation.
+    """, blank=False, max_length=510)
+    
+    def __unicode__(self):
+        return "PublicLayerList, created: %s" % (self.creation_date)
+    
+    def save(self, *args, **kwargs):
+        super(PublicLayerList, self).save(*args, **kwargs)
+        if self.active and PublicLayerList.objects.filter(active=True).count() > 1:
+            # Ensure that any previously active layer is deactivated
+            # There can be only one!
+            PublicLayerList.objects.filter(active=True).exclude(pk=self.pk).update(active=False)
+            
