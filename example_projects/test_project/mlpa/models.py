3b2720350c9a374593411cdbaf06f542b23e500c
sfletche <sfletche@gmail.com>
2011-10-06 14:47:22 -0700

    this should be the last m2m save tweak...

---
+++ example_projects/test_project/mlpa/models.py
@@ -0,0 +1,88 @@
+from django.contrib.gis.db import models
+from lingcod.features import register
+from lingcod.features.models import PointFeature, LineFeature, PolygonFeature, FeatureCollection
+from lingcod.layers.models import PrivateLayerList
+from lingcod.features.forms import FeatureForm, SpatialFeatureForm
+
+DESIGNATION_CHOICES = (
+    ('R', 'Reserve'), 
+    ('P', 'Park'),
+    ('C', 'Conservation Area')
+)
+
+###########################
+@register
+class Mpa(PolygonFeature):
+    designation = models.CharField(max_length=1, choices=DESIGNATION_CHOICES)
+    class Options:
+        verbose_name = 'Marine Protected Area'
+        form = 'mlpa.models.MpaForm'
+        manipulators = []
+        
+class MpaForm(SpatialFeatureForm):
+    class Meta(SpatialFeatureForm.Meta):
+        model = Mpa
+
+###########################
+@register
+class Array(FeatureCollection):
+    class Options:
+        form = 'mlpa.models.ArrayForm'
+        valid_children = ( 'mlpa.models.Mpa', )
+
+class ArrayForm(FeatureForm):
+    class Meta(FeatureForm.Meta):
+        model = Array
+
+###########################
+@register
+class Shipwreck(PointFeature):
+    incident = models.CharField(max_length=100,default='')
+    class Options:
+        verbose_name = 'Shipwreck'
+        form = 'mlpa.models.ShipwreckForm'
+
+class ShipwreckForm(SpatialFeatureForm):
+    class Meta(SpatialFeatureForm.Meta):
+        model = Shipwreck
+
+###########################
+@register
+class Pipeline(LineFeature):
+    type = models.CharField(max_length=30,default='')
+    diameter = models.FloatField(null=True)
+    class Options:
+        verbose_name = 'Pipeline'
+        form = 'mlpa.models.PipelineForm'
+
+class PipelineForm(SpatialFeatureForm):
+    class Meta(SpatialFeatureForm.Meta):
+        model = Pipeline
+
+###########################
+@register
+class Folder(FeatureCollection):
+    class Options:
+        form = 'mlpa.models.FolderForm'
+        valid_children = ( 
+                'mlpa.models.Mpa', 
+                'mlpa.models.Array', 
+                'mlpa.models.Pipeline', 
+                'mlpa.models.Shipwreck', 
+                'mlpa.models.Folder' 
+        )
+
+class FolderForm(FeatureForm):
+    class Meta(FeatureForm.Meta):
+        model = Folder
+
+###########################
+@register
+class UserKml(PrivateLayerList):
+    class Options:
+        form = 'mlpa.models.UserKmlForm'
+
+class UserKmlForm(FeatureForm):
+    class Meta(FeatureForm.Meta):
+        model = UserKml
+
---
+++ lingcod/staticmap/models.py
@@ -0,0 +1,49 @@
+from django.contrib.gis.db import models
+
+class MapConfig(models.Model):
+    """Model used for representing a map (ie a collection of styled map layers)
+
+        ======================  ==============================================
+        Attribute               Description
+        ======================  ==============================================
+
+        ``mapname``             Name of the map. Will be refered to by URL so 
+                                use http friendly names (no spaces, slashes, etc)
+        
+        ``mapfile``             Mapnik xml configuration file that defines data 
+                                sources and styles. All paths within xml must be 
+                                relative to media/staticmap/
+                                
+        ``default_x1``          LL X coordinate
+
+        ``default_y1``          LL Y coordinate
+
+        ``default_x2``          UR X coordinate
+
+        ``default_y2``          UR Y coordinate
+
+        ``default_width``       Default image width (pixels)
+
+        ``default_height``      Default image height (pixels)
+        ======================  ==============================================
+"""   
+    
+    def __unicode__(self):
+        return u"%s" % (self.mapfile)
+    
+    mapname = models.CharField(max_length=50,unique=True)
+    mapfile = models.FileField(upload_to='staticmap/', help_text="""
+        Mapnik xml configuration file that defines data sources and styles. 
+        All paths within xml must be relative to media/staticmap/
+    """, blank=False, max_length=510)
+    default_x1 = models.FloatField(
+            help_text="Lower-left X coordinate of default map extent. Units and spatial reference system are defined in the mapnik xml.")
+    default_y1 = models.FloatField(
+            help_text="Lower-left Y coordinate of default map extent. Units and spatial reference system are defined in the mapnik xml.")
+    default_x2 = models.FloatField(
+            help_text="Upper-right X coordinate of default map extent. Units and spatial reference system are defined in the mapnik xml.")
+    default_y2 = models.FloatField(
+            help_text="Upper-right Y coordinate of default map extent. Units and spatial reference system are defined in the mapnik xml.")
+    default_width = models.IntegerField(help_text="Default map image width in pixels")
+    default_height = models.IntegerField(help_text="Default map image height in pixels")
+    default_srid = models.IntegerField(help_text="Spatial Reference ID of the output map", default=4326)
